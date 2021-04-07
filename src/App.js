289d21ae5da568df637f1450c4e8e9b1869c6f18
Eazhilamuthan <>
2021-04-07 15:37:15 +0530

    Added Json-Server

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import Home from "./components/home.js"
 import Progress from "./components/progress.js"
 import Work from './components/works.js'
 import SinglePage from './components/singlepage.js'
-import { useEffect, useState } from "react";
-import  Homepage2x  from "./Images/Homepage@2x.jpg";
+// import { useEffect, useState } from "react";
+// import  Homepage2x  from "./Images/Homepage@2x.jpg";
 import { BrowserRouter, Route,  Switch } from "react-router-dom";
 import useFetch from "./components/useFetch.js";
 import Contact from "./components/contact.js";
 
 
 function App() {
-  const { data: Works, isPending, error} = useFetch('http://localhost:8000/blogs/')
+  const { data: Works, isPending, error} = useFetch('https://portfoliodataeazhil.herokuapp.com/blogs/')
 
   return (
     <div id="body" className=" bg-black pb-12 px-6">
